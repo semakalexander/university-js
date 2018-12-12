6fe73d5212b85ca1d9c2d16aec3e5afb6c6c7a89
skjerp <semak.alexander@gmail.com>
2018-12-12 10:59:07 +0200

    fix lab1_4

--- lab1.js
+++ lab1.js
@@ -5,3 +5,4 @@ const computeYears = birthYear => new Date().getFullYear() - birthYear;
 const getTotalPrice = (price, amount) => price * amount;
 
 const centimetersToMeters = centimeters => centimeters / 100;
+const centimetersToKiloMeters = centimeters => centimeters / 1000 / 100;
